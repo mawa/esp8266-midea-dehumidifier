104f9562825431924d32f7dee5bb1a132fc50f90
mwa <157047+mawa@users.noreply.github.com>
2024-10-22 22:08:17 +0200

    Fix compilation size by removing OTA functionality

--- src/esp8266-midea-dehumidifier.cpp
+++ src/esp8266-midea-dehumidifier.cpp
@@ -7,7 +7,7 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 //#include <SoftwareSerial.h>
-#include <ArduinoOTA.h>
+//#include <ArduinoOTA.h>
 #include <FS.h>
 #include <WiFiManager.h>
 
@@ -28,7 +28,7 @@ void handleUart();
 void getStatus();
 void handleStateUpdateRequest(String requestedState, String mode, String fanSpeed, byte humiditySetpoint);
 void loadConfig();
-void setupOTA();
+//void setupOTA();
 void publishAutoConfig();
 void saveConfig();
 void mqttCallback(char* topic, byte* payload, unsigned int length);
@@ -106,7 +106,7 @@ void setup() {
   loadConfig();
 
   setupWifi();
-  setupOTA();
+  //setupOTA();
   mqttClient.setServer(mqtt_server, 1883);
   mqttClient.setKeepAlive(10);
   mqttClient.setBufferSize(2048);
@@ -115,38 +115,38 @@ void setup() {
   mqttReconnect();
 }
 
-void setupOTA() {
-
-  ArduinoOTA.onStart([]() {
-    //Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    //Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    /*
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    */
-  });
-
-  ArduinoOTA.setHostname(identifier);
-
-  //This is less of a security measure and more a accidential flash prevention
-  ArduinoOTA.setPassword(identifier);
-  ArduinoOTA.begin();
-}
+//void setupOTA() {
+//
+//  ArduinoOTA.onStart([]() {
+//    //Serial.println("Start");
+//  });
+//  ArduinoOTA.onEnd([]() {
+//    //Serial.println("\nEnd");
+//  });
+//  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+//    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+//  });
+//  ArduinoOTA.onError([](ota_error_t error) {
+//    /*
+//      Serial.printf("Error[%u]: ", error);
+//      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+//      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+//      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+//      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+//      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+//    */
+//  });
+//
+//  ArduinoOTA.setHostname(identifier);
+//
+//  //This is less of a security measure and more a accidential flash prevention
+//  ArduinoOTA.setPassword(identifier);
+//  ArduinoOTA.begin();
+//}
 
 
 void loop() {
-  ArduinoOTA.handle();
+  //ArduinoOTA.handle();
   handleUart();
   mqttClient.loop();
 
